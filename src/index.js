ac86ddd5c57a43935d28ac53d39dcedfff3b8360
webfaisalbd <web.faisal.bd@gmail.com>
2022-11-18 23:53:57 +0600

    style added

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom/client';
 let name = "Faisal Ahmed";
 let age = "26";
 
+let nameColor = {
+  color: "lightblue"
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <h1>Hello {name}</h1>
-    <p>Age: {age}</p>
+    <h1 style={nameColor}>Hello {name}</h1>
+    <p style={{color: "lightseagreen", fontSize: "20px", fontWeight: 700}}>Age: {age}</p>
   </>
 );
