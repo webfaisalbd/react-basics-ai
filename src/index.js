0610953a0b8ad56e192ed80866301d35b91c78d5
webfaisalbd <web.faisal.bd@gmail.com>
2022-11-19 23:15:06 +0600

    date show

--- src/index.js
+++ src/index.js
@@ -8,10 +8,16 @@ let nameColor = {
   color: "lightblue"
 }
 
+const date = new Date();
+let day = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <h1 style={nameColor}>Hello {name}</h1>
     <p style={{color: "lightseagreen", fontSize: "20px", fontWeight: 700}}>Age: {age}</p>
+    <p style={{fontSize: "20px", fontWeight: 700, color: "lightsalmon"}}>{day + "/" + month + "/" + year}</p>
   </>
 );
