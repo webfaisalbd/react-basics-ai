76eb60d1e42254648b55680abf53af02eb58331d
webfaisalbd <web.faisal.bd@gmail.com>
2022-11-20 01:21:11 +0600

    make components in index.js file

--- src/index.js
+++ src/index.js
@@ -1,40 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 
+function CardComponent() {
+  return (
+    <div className="mainCard">
+      <div className='card'>
+        <h2>My ID Card</h2>
+        <div className="cardTitle">Faisal</div>
+        <div className="cardDesignation">Web Developer</div>
+        <div className="bloodGroup">AB+</div>
+        <div className="email">web.faisal.bd@gmail.com</div>
+      </div>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <div className="main-card">
-      <div className='card'>
-        <h2>My ID Card</h2>
-        <div className="card-title">Faisal</div>
-        <div className="card-designation">Web Developer</div>
-        <div className="blood-group">AB+</div>
-        <div className="email">web.faisal.bd@gmail.com</div>
-      </div>
-    </div>
-
-
-    <div className="main-card">
-      <div className='card'>
-        <h2>My ID Card</h2>
-        <div className="card-title">Faisal</div>
-        <div className="card-designation">Web Developer</div>
-        <div className="blood-group">AB+</div>
-        <div className="email">web.faisal.bd@gmail.com</div>
-      </div>
-    </div>
-
-
-    <div className="main-card">
-      <div className='card'>
-        <h2>My ID Card</h2>
-        <div className="card-title">Faisal</div>
-        <div className="card-designation">Web Developer</div>
-        <div className="blood-group">AB+</div>
-        <div className="email">web.faisal.bd@gmail.com</div>
-      </div>
+    <div className="rowCard">
+      <CardComponent />
+      <CardComponent />
+      <CardComponent />
     </div>
   </>
 );
