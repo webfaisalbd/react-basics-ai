ae5d72eb075d5a4cc284df7cf70fa37f8a13ae92
webfaisalbd <web.faisal.bd@gmail.com>
2022-11-19 23:38:06 +0600

    single card added on index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 
-let name = "Faisal Ahmed";
-let age = "26";
-
-let nameColor = {
-  color: "lightblue"
-}
-
-const date = new Date();
-let day = date.getDate();
-let month = date.getMonth();
-let year = date.getFullYear();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <h1 style={nameColor}>Hello {name}</h1>
-    <p style={{color: "lightseagreen", fontSize: "20px", fontWeight: 700}}>Age: {age}</p>
-    <p style={{fontSize: "20px", fontWeight: 700, color: "lightsalmon"}}>{day + "/" + month + "/" + year}</p>
+    <div className="main-card">
+      <div className='card'>
+        <h2>My ID Card</h2>
+        <div className="card-title">Faisal</div>
+        <div className="card-designation">Web Developer</div>
+        <div className="blood-group">AB+</div>
+        <div className="email">web.faisal.bd@gmail.com</div>
+      </div>
+    </div>
   </>
 );
