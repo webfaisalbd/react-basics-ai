d1be3c3f4947f51d8f9514c5da6a2af511b73fa5
webfaisalbd <web.faisal.bd@gmail.com>
2022-11-19 23:39:32 +0600

    multiple card added in index.js with code repeatation

--- src/index.js
+++ src/index.js
@@ -14,5 +14,27 @@ root.render(
         <div className="email">web.faisal.bd@gmail.com</div>
       </div>
     </div>
+
+
+    <div className="main-card">
+      <div className='card'>
+        <h2>My ID Card</h2>
+        <div className="card-title">Faisal</div>
+        <div className="card-designation">Web Developer</div>
+        <div className="blood-group">AB+</div>
+        <div className="email">web.faisal.bd@gmail.com</div>
+      </div>
+    </div>
+
+
+    <div className="main-card">
+      <div className='card'>
+        <h2>My ID Card</h2>
+        <div className="card-title">Faisal</div>
+        <div className="card-designation">Web Developer</div>
+        <div className="blood-group">AB+</div>
+        <div className="email">web.faisal.bd@gmail.com</div>
+      </div>
+    </div>
   </>
 );
