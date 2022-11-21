5db09b87110f891077e4e83dff8bfd5857100cb9
webfaisalbd <web.faisal.bd@gmail.com>
2022-11-22 01:08:22 +0600

    mapping data to components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import Card from "./components/Card";
 
+import Data from './data.json';
+// console.log(Data[0]);
+
 function App() {
+
+    // let items = [];
+    // for(let i=0; i<Data.length;i++){
+    //     items.push(<Card title={Data[i].title} desc={Data[i].desc} />)
+    // }
+
+    // let items = [];
+    // items = Data.map((item) => <Card title={item.title} desc={item.desc} />)
+
     return (
         <div className="rowCard">
-            <Card />
-            <Card />
-            <Card />
+           {
+            Data.map((item, index) => <Card key={index} title={item.title} desc={item.desc} /> )
+           }
         </div>
     );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,13 @@
-function Card() {
+
+function Card(props) {
+    // console.log(props);
+    const { title, desc} = props;
+
     return (
       <div className="mainCard">
         <div className='card'>
-          <h2>My ID Card</h2>
-          <div className="cardTitle">Faisal</div>
-          <div className="cardDesignation">Web Developer</div>
-          <div className="bloodGroup">AB+</div>
-          <div className="email">web.faisal.bd@gmail.com</div>
+          <div className="cardTitle">{title}</div>
+          <div className="cardDesignation">{desc}</div>
         </div>
       </div>
     );
