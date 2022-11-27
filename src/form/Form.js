efb78277d0a1da71cf6bf7ee292ec5b6e6dba46e
webfaisalbd <web.faisal.bd@gmail.com>
2022-11-27 21:12:43 +0600

    form control using react useState in input field attribute(value)

--- src/form/Form.js
+++ src/form/Form.js
@@ -1,20 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './form.module.css'
 
 const Form = () => {
 
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     const handleName = (e) => {
-        console.log(e.target.value);
+        setName(e.target.value);
     }
     const handleEmail = (e) => {
-        console.log(e.target.value);
+        setEmail(e.target.value);
     }
     const handlePassword = (e) => {
-        console.log(e.target.value);
+        setPassword(e.target.value);
     }
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("hello");
+        console.log(name, email, password);
     }
 
 
@@ -24,15 +28,18 @@ const Form = () => {
             <form onSubmit={handleSubmit}>
                 <div className={style.formGroup}>
                     <label htmlFor="name">Name: </label>
-                    <input type="text" name="name" id="name" required onChange={handleName} />
+                    <input type="text" name="name" id="name" required onChange={handleName}
+                    value={name} />
                 </div>
                 <div className={style.formGroup}>
                     <label htmlFor="email">Email: </label>
-                    <input type="email" name="email" id="email" required onChange={handleEmail} />
+                    <input type="email" name="email" id="email" required onChange={handleEmail}
+                    value={email} />
                 </div>
                 <div className={style.formGroup}>
                     <label htmlFor="password">Password: </label>
-                    <input type="password" name="password" id="password" required onChange={handlePassword} />
+                    <input type="password" name="password" id="password" required onChange={handlePassword}
+                    value={password} />
                 </div>
                 <div>
                     <button type='submit'>Register</button>
